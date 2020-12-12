534cd0efee7fefc06ab28b8ee4736af1e54059f3
Bryan Hann <nobody@nowhere>
2020-12-12 12:28:40 +0800

    Refactor with lib-misc.
    Move to [lib-misc] all code that does more than define [borg_*]
    functions. Replace the single [runonce()] call with several calls
    to the new dunder function [__once__()]. Using [__once__()], move
    code that affects the system to [lib-misc].

--- bashrc.sh
+++ bashrc.sh
@@ -1,16 +1,27 @@
-bashrc_once () {
+__note__ () {
+  echo "$(__trace__ 1 8 --ignore-source)" $*
+}
+
+__path_append__() {
+   export PATH=${PATH}:$1
+}
+
+__once__ () {
     [ -z "${BORG_RPID}" ] || return 0
-    __run__ __export__ BORG_RPID $$
-    __run__ __export__ PATH      ${PATH}:~/.local/bin
+    $*
+}
+
+__end_once__ () {
+  __once__ __run__ __export__ BORG_RPID $$
 }
 
 bashrc_main () {
     source ${BORG}/dunder.sh
     __banner__ BORG/bashrc BEGIN 0.1
-    bashrc_once
     __src__ ${BORG}/lib-borg
-    __run__ borg_gitprompt_install_monty
+    __src__ ${BORG}/lib-misc
     __run__ borg_deeper -v
+    __end_once__
     __banner__ BORG/bashrc END
 }
 
