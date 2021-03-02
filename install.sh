1af8b511e1ab5ebfae3a2b635ebd108d84f0e6e2
Bryan Hann <nobody@nowhere>
2021-03-03 00:00:29 +0800

    Revamp installation and sourcing

---
+++ install.sh
@@ -0,0 +1,25 @@
+[ "$0" == "./install.sh" ] || { echo 'try ./install.sh'; exit; }
+
+mycopy () {
+    local backups=${HOME}/.backups
+    local name=$1
+    local src=files/${name}
+    local dst=${HOME}/.${name}
+    local backup0=${backups}/orig.${name}
+    local backup1=${backups}/$(date "+%Y-%m-%d-T-%H-%M-%S").${name}
+    [ ! -f ${dst}     ] && echo creating ${dst} && cp ${src} ${dst} && return
+    cmp -s $src $dst    && echo ignoring ${dst}                     && return
+    [ ! -f ${backup0} ] && cp $dst $backup0
+    true                && cp $dst $backup1
+    true                && echo replacing ${dst} && cp ${src} ${dst} && return
+}
+
+for name in $(/bin/ls files); do mycopy $name; done
+
+mkdir -p    ${HOME}/.config/myborg
+echo $PWD > ${HOME}/.config/myborg/current.txt
+cat       > ${HOME}/.config/myborg/dothandler << HEREDOC
+echo MYBORG: sourcing ~/.config/myborg/dothandler \$*
+export BORG=\$(cat \${HOME}/.config/myborg/current.txt)
+source \${BORG}/dothandler \$*
+HEREDOC
