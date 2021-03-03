29f16f6c4452fe2a17d73c0da776713c42af6e13
Your Name <you@example.com>
2021-03-03 13:05:12 +0800

    BUGFIX: Create [~/.backups].

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ mycopy () {
     local dst=${HOME}/.${name}
     local backup0=${backups}/orig.${name}
     local backup1=${backups}/$(date "+%Y-%m-%d-T-%H-%M-%S").${name}
+    mkdir -p ${backups}
     [ ! -f ${dst}     ] && echo creating ${dst} && cp ${src} ${dst} && return
     cmp -s $src $dst    && echo ignoring ${dst}                     && return
     [ ! -f ${backup0} ] && cp $dst $backup0
