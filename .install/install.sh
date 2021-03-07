3994430d185a79c03d548d24646d73075c20d4e3
Bryan Hann <nobody@nowhere>
2021-03-07 21:20:13 +0800

    Enhance installer.
    The installer can now remove the current [~/.local/var/myborg] for
    a complete reinstallation. Four new borg scripts are supplied to
    streamline the installer code:
            [./bin/borg-backup]    - back up a file with timestamp
            [./bin/borg-cmp]       - compare two files
            [./bin/borg-hasoption] - allows optiong checking
            [./bin/borg:insitu]    - execute a command insitu

--- .install/install.sh
+++ .install/install.sh
@@ -1,21 +1,42 @@
 [ "$0" == "./install.sh" ] || { echo 'try ./install.sh'; exit; }
+ARGS=$*
+opt () { borg-hasoption $1 $ARGS; return $?; }
+vard=${HOME}/.local/var/myborg
 
-mycopy () {
-    local backups=${HOME}/.backups
-    local name=$1
-    local src=files/${name}
-    local dst=${HOME}/.${name}
-    local backup0=${backups}/orig.${name}
-    local backup1=${backups}/$(date "+%Y-%m-%d-T-%H-%M-%S").${name}
-    mkdir -p ${backups}
-    [ ! -f ${dst}     ] && echo creating ${dst} && cp ${src} ${dst} && return
-    cmp -s $src $dst    && echo ignoring ${dst}                     && return
-    [ ! -f ${backup0} ] && cp $dst $backup0
-    true                && cp $dst $backup1
-    true                && echo replacing ${dst} && cp ${src} ${dst} && return
+remove_vard () {
+    cat ./warning.txt
+    opt -y || { read; read; read; }
+    rm -rf ${vard}
+    echo [${vard}] has been removed
 }
 
-newborg=$(dirname $PWD)
-for name in $(/bin/ls files); do mycopy $name; done
-mkdir -p    ${HOME}/.local/var/myborg
-echo ${newborg} > ${HOME}/.local/var/myborg/current.txt
+copyfiles () {
+    for name in $(/bin/ls files); do
+        src=files/${name}
+        dst=${HOME}/.${name}
+        dir=${HOME}/.back
+        borg-backup ${dst} ${dir}
+        echo overwriting: ${dst}
+        cp ${src} ${dst}
+    done
+}
+
+write_index () {
+    mkdir -p ${vard}
+    index=${vard}/current.txt
+    echo $(dirname $PWD) > ${index}
+    echo index : [${index}] is [$(cat $index)].
+}
+
+case ${1}. in
+    install.)
+        opt -v && remove_vard
+        copyfiles
+        write_index
+        cat ./outro.txt
+        ;;
+    *)
+        cat ./usage.txt
+        ;;
+esac
+
--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
 [ "$0" == "./install.sh" ] || { echo 'try ./install.sh'; exit; }
-> /dev/null pushd ./.install
-./install.sh
-> /dev/null popd
+export PATH=${PWD}/bin:${PATH}
+. borg:insitu .install ./install.sh $*
